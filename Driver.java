036e925240e71b0d92164b513e60a24467dd98d2
Ch4rIes <55766787+Ch4rIes@users.noreply.github.com>
2021-09-29 10:00:25 -0700

    Add files via upload

---
+++ Driver.java
@@ -0,0 +1,9 @@
+import java.util.*;
+//author: Charles Zuo
+
+public class Driver {
+    public static void main(String[] args){
+        Calculator c = new Calculator();
+        c.display(1 , 5 , 6 , 0 , 2 , 0 , 3 , 1 , 1 , 5 , 1 , 2 , 3);
+    }
+}
